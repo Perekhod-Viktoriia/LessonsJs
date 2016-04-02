4a3c2567aa94f3403d3fb6cfaddfcf6e379849e9
Perekhod-Viktoriia <viktoria281294@gmail.com>
2016-04-02 21:57:13 +0300

    second task

--- Vetal/script.js
+++ Vetal/script.js
@@ -1,25 +1,66 @@
-'use strict';
-//Пометка о том, что скрипт запущен.
-var flagDone = false;
-// оброботчик сообытия нажатия кнопки
-window.addEventListener('keydown', function (e)
+'use strict'
+document.addEventListener("DOMContentLoaded", function(){
+    var flagDone = false;
+    // Вытащить список "ul"
+    var Sum =document.querySelector('#Sum');
+    var Sort =document.querySelector('#Sort');
+    var Rand =document.querySelector('#Rand');
+    var ul = document.querySelector('ul');
+    var steck= true;
+    
+    Sum.addEventListener ('click', function(){
+        if (flagDone ==false)
+            {
+                var prises = CreateMass();
+                var sum=0;
+                prises.forEach(function (el){
+                    sum += el.price; 
+                } );
+                 var  li = document.createElement ('li');
+                li.innerHTML = 'Cумма: ' + sum ; 
+                ul.appendChild(li);
+                flagDone = true;
+            }
+    });
+    
+   Sort.addEventListener ('click', function(){
+       var prices = CreateMass ();
+       if (steck==true)
+           {
+            prices.sort(function (a,b)
+                    {
+                        return  b.price-a.price;
+                    });
+                steck =false;         
+           }
+       else 
+           {
+            prices.sort(function (a,b)
+                    {
+                        return  a.price-b.price;
+                    });
+               steck =true;
+           }
+       createList(prices);
+    });
+    
+    Rand.addEventListener ('click', function(){
+        var prices = CreateMass ();
+        prices.sort(function (){
+            return Math.random() - 0.5 ;
+        });
+        createList(prices);
+    });
+    
+    function CreateMass()
     {
-    // проверка на нажатие кнопки "Enter"
-        if (e.keyCode == 13 && flagDone === false )
-        {
-            // Вытащить все елементы списка
-            var element = document.querySelectorAll('li');
-            var i=0;
-            var innerHTML;
-            var returnNum; 
-            var  prices = [];
-            var counterPrice=0;
-            var elementPlusPlus;
-            var li;
-            // Вытащить список "ul"
-            var ul = document.querySelector('ul');
-            
-            while (i < element.length )
+        var i = 0;
+        let  prices = [];
+        let elementPlusPlus;
+        let element = document.querySelectorAll('li');
+        let innerHTML;
+        let returnNum;
+        while (i < element.length )
             {
                 elementPlusPlus = element[i++];
                 // Вытаскиваем текстовое значение
@@ -29,28 +70,24 @@ window.addEventListener('keydown', function (e)
                 // Проверка является ли переменная массивом
                 returnNum = returnNum instanceof Array  ?  +returnNum[0] : 0 ; 
                 //Записываем массив с ключами и их значениями 
-                prices.push({price:returnNum, text:innerHTML});
-                counterPrice +=returnNum;   
-                //Берем елемент, вызываем родителя и уаляем именно этот елемент
-                elementPlusPlus.parentNode.removeChild(elementPlusPlus);        
+                prices.push({price:returnNum, text:innerHTML});  
+                //Берем елемент, вызываем родителя и уаляем именно этот елемент       
             }
-            prices.sort(function (a,b)
-                {
-                    return  b.price-a.price;
-                });
-            i=0;
-            //Создаем новый список елементов
-            while ( i < prices.length)
-                {
-                    li = document.createElement('li');
-                    li.innerHTML = prices[i++].text;
-                    ul.appendChild(li);   
-                }
-            // Добовляем сумму товаров
-            li = document.createElement ('li');
-            li.innerHTML = 'Cумма: ' + counterPrice ; 
-            ul.appendChild(li);
-            //конец работы
-            flagDone = true;
-        }
-    });
+        return prices;
+    }
+    function createList (prices)
+    {
+        let element = document.querySelectorAll('li');
+        //var prices = CreateMass ();
+        var i = 0;
+        var ul = document.querySelector('ul'); 
+        var li;
+        while ( i < prices.length)
+            {
+                var old = element[i];
+                li = document.createElement('li');
+                li.innerHTML = prices[i++].text;
+               old.parentNode.replaceChild(li ,old);
+            }
+    }
+});
